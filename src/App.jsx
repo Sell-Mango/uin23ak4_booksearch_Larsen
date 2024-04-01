fa3969922929c55ce8b100f23e4e2fbd88822b87
Kjell-Magne <kjell_m.larsen@yahoo.com>
2024-04-01 14:13:31 +0200

    Startet på API tilkobling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,30 @@ import testimg from './img/5cbe235ddaa9a96cfe352384.webp'
 
 function App() {
 
+  const cover_img_url = "https://covers.openlibrary.org/a/olid/"
+
+  const [numResults, setNumResults] = useState()
+  const [content, setContent] = useState([])
+  const [query, setQuery] = useState("james bond")
+
+  const getData = async(title) => {
+    try {
+      const response = await fetch(`https://openlibrary.org/search.json?title=${title}`)
+      const data = await response.json()
+      console.log(data.docs)
+      
+      setNumResults(data.numFound)
+      setContent(data.docs)
+    }
+    catch {
+      console.error("det har skjedd en feil")
+    }
+  }
+
+  useEffect(() => {
+    getData(query)
+  }, [])
+
   return (
     <>
       <header>
@@ -16,34 +40,23 @@ function App() {
         </form>
       </header>
       <main>
-        <p className="searchResults">Showing <span>6</span> results for: <span>James Bond</span></p>
-        <article className="bookCard">
-          <section>
-            <span className="rating">Rating: 4.5/6</span>
-            <h2>Casino Royale</h2>
-            <p>Author: <span>Ian Flemming</span></p>
-            <p>First published: <span>1953</span></p>
-            <a href="#" className="btn btn-m">Read more</a>
-          </section>
-          <button><img src={star_outline} alt="" /></button>
-          <picture>
-            <img src={testimg} alt="" />
-          </picture>
-        </article>
+        <p className="searchResults">Showing {numResults} results for: {query}</p>
+        {content.map((book) => (
+          <article className="bookCard" key={book.key}>
+            <section>
+              <span className="rating">Rating: {book.ratings_average}/6</span>
+              <h2>{book.title}</h2>
+              <p>Author: {book.author_name}</p>
+              <p>First published: {book.first_publish_year}</p>
+              <a href="#" className="btn btn-m">Read more</a>
+            </section>
+            <button><img src={star_outline} alt="" /></button>
+            <picture>
+              <img src={`https://covers.openlibrary.org/a/olid/${book.cover_edition_key}.jpg`} alt="" />
+            </picture>
+          </article>
 
-        <article className="bookCard">
-          <section>
-            <span>4.5/6</span>
-            <h2>Casino Royale</h2>
-            <p>Author: <span>Ian Flemming</span></p>
-            <p>First published: <span>1953</span></p>
-            <a href="#" className="btn btn-m">Read more</a>
-          </section>
-          <button><img src={star_outline} alt="" /></button>
-          <picture>
-            <img src={testimg} alt="" />
-          </picture>
-        </article>
+        ))}
 
       </main>
       <footer>
